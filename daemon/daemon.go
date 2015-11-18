90f0e3439b8dbb4cacc1a3d627bb8b034b60c8ad
C@Z <cz.theng@gmail.com>
2015-11-18 14:05:19 +0800

    New daemon

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,126 +1,99 @@
 /**
 * Daemon for golang
-*/
+ */
 
-package daemon 
+package daemon
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 	"strconv"
 	"syscall"
-	"os/signal"
-	"os"
 )
 
-func dealSignal(c chan os.Signal){
-	signo := <- c
-	if signo == syscall.SIGHUP || signo == syscall.SIGINT {
-		fmt.Println("sighub or sigint")
-	}
+type MainFunc func()
+
+//Context is a Dameon Context
+type Context struct {
+	lockFile string
+	pidFile  string
+	process  *os.Process
+	main     MainFunc
 }
 
-func Boot(lockFilePath,pidFilePath string){
-	var pid  uintptr
-	var errno syscall.Errno
-	var err error
-	daemonCh := make(chan os.Signal, 1)
+//Daemon run a daemon process
+func (ctx *Context) Daemon() (err error) {
+	return
+}
 
-	// fork
-	pid, ret2, errno := syscall.RawSyscall(syscall.SYS_FORK,0,0,0)
-	if (errno != 0){
-		panic(err)
-	}
-	
-	if (runtime.GOOS == "darwin") && (ret2 == 1){
-		pid = 0
+func Boot(lockFilePath, pidFilePath string, main MainFunc) {
+	ctx := &Context{
+		lockFile: lockFilePath,
+		pidFile:  pidFilePath,
+		main:     main,
 	}
 
-	if pid >0 {
-		// parent just exit
-		os.Exit(0)
-	}
+	ctx.Daemon()
 
-	// set sid
-	_,err = syscall.Setsid()
-	if err != nil {
-		panic(err)
-	}
-	
-	// fork twice
-/* darwin can't fork twice
-	pid, _, errno = syscall.RawSyscall(syscall.SYS_FORK,0,0,0)
-	if (errno != 0){
-		panic(err)
-	}
-
-	if (runtime.GOOS == "darwin") && (ret2 == 1){
-		pid = 0
-	}
-	if pid >0 {
-		// parent just exit
-		os.Exit(0)
-	}
-	fmt.Print("Grand child")
-*/
 	// flock
-	lockFd,err:= syscall.Open(lockFilePath,syscall.O_CREAT | syscall.O_WRONLY,syscall.S_IRUSR | syscall.S_IWUSR)
+	lockFd, err := syscall.Open(lockFilePath, syscall.O_CREAT|syscall.O_WRONLY, syscall.S_IRUSR|syscall.S_IWUSR)
 	if err != nil {
 		panic(err)
 	}
 
-	err = syscall.Flock(lockFd,syscall.LOCK_EX|syscall.LOCK_NB)
+	err = syscall.Flock(lockFd, syscall.LOCK_EX|syscall.LOCK_NB)
 	if err != nil {
 		fmt.Println("The Process is already runing ")
 		os.Exit(-1)
 	}
-
+	println("lock success")
 	// pid file
-	pidFd,err:= syscall.Open(pidFilePath,syscall.O_CREAT | syscall.O_WRONLY,syscall.S_IRUSR | syscall.S_IWUSR)
+	pidFd, err := syscall.Open(pidFilePath, syscall.O_CREAT|syscall.O_WRONLY, syscall.S_IRUSR|syscall.S_IWUSR)
 	if err != nil {
 		panic(err)
 	}
 	_curPid := strconv.Itoa(os.Getpid())
-	syscall.Write(pidFd,[]byte(_curPid))
+	syscall.Write(pidFd, []byte(_curPid))
 	syscall.Close(pidFd)
+	println("pid success")
 	// chdir
-	curDir,err := os.Getwd()
+	curDir, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	err  = os.Chdir(curDir)
+	err = os.Chdir(curDir)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// umask
-	syscall.Umask(0x0000)
+	//syscall.Umask(0x0000)
 
-	// close 
-	syscall.Close(syscall.Stdin)
-	syscall.Close(syscall.Stdout)
-	syscall.Close(syscall.Stderr)
+	// close
+	/*
+		syscall.Close(syscall.Stdin)
+		syscall.Close(syscall.Stdout)
+		syscall.Close(syscall.Stderr)
 
+		// redict
+		fd, err := syscall.Open("/dev/null", syscall.O_RDWR, 0)
+		if err != nil {
+			panic(err)
+		}
+		syscall.Dup2(fd, syscall.Stdin)
+		syscall.Dup2(fd, syscall.Stdout)
+		syscall.Dup2(fd, syscall.Stderr)
 
-	// redict
-	fd,err := syscall.Open("/dev/null",syscall.O_RDWR,0)
-	if err!= nil {
-		panic(err)
-	}
-	syscall.Dup2(fd,syscall.Stdin)
-	syscall.Dup2(fd,syscall.Stdout)
-	syscall.Dup2(fd,syscall.Stderr)
-
-
+	*/
 	// signal
-	signal.Notify(daemonCh,syscall.SIGHUP,syscall.SIGINT)
-	go dealSignal(daemonCh)
-
+	//signal.Ignore(syscall.SIGHUP, syscall.SIGINT)
+	//go dealSignal(daemonCh)
 
 	// setuid
 
 	// setgid
 
 	// do process
-
+	println("boot success")
 }
